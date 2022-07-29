df108faaa8d95867eddbd1d5a199d716480baf6c
soohouse <misowltn@gmail.com>
2022-07-29 17:36:49 +0900

    2022-07-29 게시판 VO 생성

---
+++ src/main/java/com/spring/leaf/notice/mapper/INoticeMapper.java
@@ -0,0 +1,22 @@
+package com.spring.leaf.notice.mapper;
+
+import com.spring.leaf.notice.command.NoticeVO;
+
+//공지사항 매퍼 인터페이스 : 2022-07-29 생성
+
+public interface INoticeMapper {
+	
+	//글 등록
+	void regist(NoticeVO vo);
+	
+	//글 목록
+	
+	//총 게시물 수
+	
+	//상세보기
+	
+	//수정
+	
+	//삭제
+
+}
