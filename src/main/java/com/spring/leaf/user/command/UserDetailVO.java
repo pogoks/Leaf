6237cc28c3de9cf221cfe4e5a9f20824d6be11f3
Pogoks <kbjun97@naver.com>
2022-08-12 22:36:36 +0900

    2022-08-12 내정보 수정 및 이력서 수정

--- src/main/java/com/spring/leaf/user/command/UserDetailVO.java
+++ src/main/java/com/spring/leaf/user/command/UserDetailVO.java
@@ -1,31 +1,23 @@
 package com.spring.leaf.user.command;
 
-import java.sql.Timestamp;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-
-// 일반회원 VO : 2022-07-27 생성
-
 @Getter
 @Setter
 @ToString
-public class UserVO {
+public class UserDetailVO {
 
 	private int userNO;
 	private String userID;
-	private String userPW;
 	private String userName;
 	private String userEmail1;
 	private String userEmail2;
 	private String userPhone1;
 	private String userPhone2;
 	private String userPhone3;
-	private Timestamp userDate;
 	private String userIntro;
-	private Timestamp userDelete;
-	private String commonCODE;
+	private String resumeRealname;
 	
 }
